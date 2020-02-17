fe0cedceb538640b3d241a83d172f4b873e9cc92
Mateo Garcia <mateogarcialopez3@gmail.com>
2020-02-16 21:11:39 -0500

    guardado de mensaje principal

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+$(document).ready(function () {
+  setTimeout(function () {
+    $("#msgPrincipal").fadeOut(1500);
+  }, 3000);
+});
+
+
 $(function () {
 
   var flag = 0;
+  var flag1 = 0;
 
   $('.share').on('click', function () {
     if (flag == 0) {
       $('.one iframe').delay().fadeIn();
       document.getElementById("ifr1").style.display = "block";
-      document.getElementById("msgPrincipal").style.display = "block";
+      if (flag1 == 0) {
+        document.getElementById("msgPrincipal").style.display = "block";
+      }else{
+        document.getElementById("msgPrincipal").style.display = "none";
+      }
       document.getElementById("ifr1").style.opacity = "3";
       flag = 1;
+      flag1 = 1;
     }
 
 
@@ -17,17 +30,10 @@ $(function () {
       document.getElementById("ifr1").style.display = "none";
       document.getElementById("msgPrincipal").style.display = "none";
       flag = 0;
-
-      $('#msgPrincipal').delay().fadeOut();
     }
   });
 });
 
 
-$(document).ready(function () {
-  setTimeout(function () {
-    $("#msgPrincipal").fadeOut(1500);
-  }, 3000);
-});
 
 
