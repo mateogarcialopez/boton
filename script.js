39617e5cbdd3cc102e8579dc5e647b0bb3a8a0d1
mateogarcialopez <mateogarcialopez3@gmail>
2020-02-18 08:27:13 -0500

    modal listo

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+//Desaparecer en mensaje principal del chat despues de x segundos
 $(document).ready(function () {
   setTimeout(function () {
     $("#msgPrincipal").fadeOut(1500);
-  }, 6000);
+  }, 5000);
 });
 
 
@@ -10,28 +11,25 @@ $(function () {
   var flag = 0;
   var flag1 = 0;
 
-
-  $('#imgchec').on('click', function () {
-    document.getElementById('modal').style.display = 'none';
-    document.getElementById("ifr1").style.display = "block";
-    document.getElementById("btnCerrar").style.display = "block";
-    flag = 0;
-    console.log(flag);
-  });
-
-
+  //Funcion para cerrar el chat desde la X
   $('#btnCerrar').on('click', function () {
     document.getElementById('ifr1').style.display = 'none';
     flag = 0;
   });
 
+//controlar el abrir y cerrar el chat desde el boton
+  $('.share, .contenido').on('click', function () {
 
-  $('.share').on('click', function () {
-    console.log(flag);
     if (flag == 0) {
       $('.one iframe').delay().fadeIn();
+
+      //---------------cerrar la ventana modal-----------------------------
+      document.getElementById("modal").style.display = "none";
+      //---------------cerrar la ventana modal-----------------------------
+      
       document.getElementById("ifr1").style.display = "block";
       document.getElementById("btnCerrar").style.display = "block";
+
       if (flag1 == 0) {
         document.getElementById("msgPrincipal").style.display = "block";
       } else {
