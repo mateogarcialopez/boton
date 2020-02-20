6a6bf35b7464220d3bc971531e85d9e49b17059d
Juan Camilo Cardona <prjchec.jcardona@umanizales.edu.co>
2020-02-20 15:26:14 -0500

    fixed for send chec

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,15 @@
+import { opens_chat, dialog_id } from "./config";
+
 //Ocultar el mensaje principal del chat despues de x segundos
 $(document).ready(function () {
-  setTimeout(function () {
-    $("#msgPrincipal").fadeOut(1500);
-  }, 15000);
+  
 });
 
 
 $(function () {
 
-
   var flag = 0;
-  var flag1 = 0;
+  var to;
 
   //Funcion para cerrar el chat desde la (X)
   $('#btnCerrar').on('click', function () {
@@ -19,34 +18,29 @@ $(function () {
   });
 
 //Controlar el abrir y cerrar el chat desde el boton
-  $('.share, .contenido').on('click', function () {
+    opens_chat.on('click', function () { 
+      $("#msgPrincipal").show();
 
     if (flag == 0) {
       $('.one iframe').delay().fadeIn();
 
-      //---------------cerrar la ventana modal-----------------------------
-      document.getElementById("modal").style.display = "none"; //Reemplazar el ID del modal
-      //---------------cerrar la ventana modal-----------------------------
+      document.getElementById(dialog_id).style.display = "none"; 
       
       document.getElementById("ifr1").style.display = "block";
       document.getElementById("btnCerrar").style.display = "block";
 
-      if (flag1 == 0) {
-        document.getElementById("msgPrincipal").style.display = "block";
-      } else {
-        document.getElementById("msgPrincipal").style.display = "none";
-      }
+      to = setTimeout(function () {
+        $("#msgPrincipal").fadeOut(1500);
+      }, 15000);
+
       document.getElementById("ifr1").style.opacity = "3";
       flag = 1;
-      flag1 = 1;
-    }
-
-
-    else {
+    }else {
       $('.one iframe').delay().fadeOut();
       document.getElementById("ifr1").style.display = "none";
       document.getElementById("msgPrincipal").style.display = "none";
       flag = 0;
+      clearTimeout(to);
     }
   });
 });
